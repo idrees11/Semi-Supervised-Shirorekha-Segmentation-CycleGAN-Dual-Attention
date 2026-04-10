9eb0a81f754451d8fdf56fc5ee899e61111d18bd
Mohammad Idrees Bhat <29601026+idrees11@users.noreply.github.com>
2026-04-10 16:29:08 +0530

    Update discriminator.py

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,1 +1,75 @@
 
+import torch
+import torch.nn as nn
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(EncoderBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+    
+    def forward(self, x):
+        return self.conv(x)
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x, skip):
+        x = self.conv(x)
+        x = x + skip
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=1):
+        super(Discriminator, self).__init__()
+        
+        # Encoder layers
+        self.e1 = EncoderBlock(in_channels, 64)
+        self.e2 = EncoderBlock(64, 128)
+        self.e3 = EncoderBlock(128, 256)
+        self.e4 = EncoderBlock(256, 512)
+        
+        # Bottleneck
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        
+        # Decoder layers
+        self.d1 = DecoderBlock(1024, 512)
+        self.d2 = DecoderBlock(512, 256)
+        self.d3 = DecoderBlock(256, 128)
+        self.d4 = DecoderBlock(128, 64)
+        
+        # Final output layer
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, stride=1, padding=0, bias=False)
+
+    def forward(self, x):
+        # Encoder forward pass
+        s1 = self.e1(x)
+        s2 = self.e2(s1)
+        s3 = self.e3(s2)
+        s4 = self.e4(s3)
+        
+        # Bottleneck
+        b = self.bottleneck(s4)
+        
+        # Decoder with skip connections
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+        
+        # Final output
+        x = self.outputs(d4)
+        return x
